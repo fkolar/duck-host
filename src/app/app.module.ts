9c14a4b9316652c3dfedd925e676f80c6cbb7c34
fkolar <fkolar@icloud.com>
2021-11-09 22:32:39 +0100

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {Route, RouterModule} from '@angular/router';
+import {loadRemoteModule} from '@angular-architects/module-federation'
+
+const routes: Route[] = [
+  {
+    path: 'game',
+    loadChildren: () =>
+      loadRemoteModule({
+        remoteEntry: 'http://localhost:3003/remoteEntry.js',
+        remoteName: 'duckhunt',
+        exposedModule: './Module'
+      }).then((m) => m.DuckHuntModule)
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    RouterModule.forRoot(routes, {initialNavigation: 'enabled'}),
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
